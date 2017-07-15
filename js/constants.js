a4e4850868105a80133352fd9a1d0422d4049000
yinm <yinm.cminor9@gmail.com>
2017-07-15 12:10:48 +0900

    add constants.js

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = Object.freeze({
+  appId: '',
+  dynamoDBTableName: 'LongFormAudioSample',
+  states: {
+    START_MODE: '',
+    PLAY_MODE: '_PLAY_MODE',
+    RESUME_DECISION_MODE: '_RESUME_DECISION_MODE'
+  }
+});
