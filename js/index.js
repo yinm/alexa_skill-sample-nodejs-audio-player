e7a6059a8c0910e149e9ff94bce010f049cd3387
yinm <yinm.cminor9@gmail.com>
2017-07-15 12:07:23 +0900

    add index.js

---
+++ js/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+const constants = require('./constants');
+const stateHandlers = require('./stateHandlers');
+const audioEventHandlers = require('./audioEventHandlers');
+
+exports.handler = (event, context, callback) => {
+  const alexa = Alexa.handler(event, context);
+  alexa.appId = constants.appId;
+  alexa.dynamoDBTableName = constants.dynamoDBTableName;
+  alexa.registerHandlers(
+    stateHandlers.startModeIntentHandlers,
+    stateHandlers.playModeIntentHandlers,
+    stateHandlers.remoteControllerHandlers,
+    stateHandlers.resumeDecisionModeIntentHandlers,
+    audioEventHandlers
+  );
+  alexa.execute();
+};
